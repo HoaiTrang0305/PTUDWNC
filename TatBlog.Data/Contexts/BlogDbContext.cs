0dbf67b65e1aa6e1f236aaa7a2a3304d0f2da740
Trang cute <trang0305pham@gmail.com>
2023-03-01 15:44:47 +0700

    Lab01 B. Homework

---
+++ TatBlog.Data/Contexts/BlogDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TatBlog.Core.Entities;
+using TatBlog.Data.Mappings;
+
+namespace TatBlog.Data.Contexts
+{
+    public class BlogDbContext:DbContext
+    {
+        public DbSet<Author> Author { get; set; }
+
+        public DbSet<Category> Category { get; set; }
+
+        public DbSet<Post> Post { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=MSI\HOAITRANG;Database=TatBlog;
+            Trusted_Connection=True;MultipleActiveResultSets=true;Encrypt=false");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(
+                typeof(CategoryMap).Assembly);
+        }
+    }
+}
