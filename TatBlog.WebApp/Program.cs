cf58a9c97c67b9c872b8c5dfab8a992942e1e47f
HoaiTrang0305 <2014496@dlu.edu.vn>
2023-03-08 15:58:09 +0700

    lab02-6

--- TatBlog.WebApp/Program.cs
+++ TatBlog.WebApp/Program.cs
@@ -1,6 +1,28 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
+{
+	//Thêm các dịch vụ được yêu cầu bới MVC Framework
+	builder.Services.AddControllersWithViews();
+}
 var app = builder.Build();
+{
+	//Cấu hình HTTPS Request pipeline
+	//Thêm middleware để hiển thị thông tin báo lỗi
+	if (app.Environment.IsDevelopment())
+	{
+		app.UseDeveloperExceptionPage();
+	}
+	else
+	{
+		app.UseExceptionHandler("/Blog/Error");
+		//Thêm middleware cho việc áp dụng HSTS(Thêm header strict-transport-security vào HTTP response)
+		app.UseHsts();
+	}
+	app.UseHttpsRedirection();
+	app.UseStaticFiles();
+	app.UseRouting();
 
-app.MapGet("/", () => "Hello World!");
+	app.MapControllerRoute(name: "default",
+		pattern: "{controller=Blog}/{action=Index}/{id?}");
+}
 
 app.Run();
